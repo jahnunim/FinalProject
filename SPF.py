67d9ec640c9660c761ca2035504828d0b3b73146
ronv <ronvalensi@gmail.com>
2017-12-21 16:25:54 +0200

    Main - 0.1

--- SPF.py
+++ SPF.py
@@ -1,43 +1,44 @@
 # the SPF.PY tests if the domain has SPF record registered & if it configures as HardFail.
 
+# Imports the dns module.
 import dns.resolver
 
-print(dir(dns.resolver))
 
-import sys
-print(sys.path)
 
-# SPF score
+# Reset SPF record & score
 score = 0
+spfRecord = None
+
+# Requests from the user the domain to check
+domain = input("What's the domain you wish to check his SPF record?")
+
 
 try:
-    answers = dns.resolver.query('valensiweb.com', 'TXT')
+    # Querying for the domain's TXT records
+    answers = dns.resolver.query(domain, 'TXT')
 
+    # Enumarates through all of the TXT records and pulls the SPF record
     for rdata in answers:
         if "v=spf" in rdata.to_text():
             spfRecord = rdata.to_text()
-            score+= 1
-            print('SPF in place',spfRecord)
-            if "-all" in rdata.to_text():
-                print("HardFail inplace")
-                score+=1
-            else:
-                print("SoftFail inplace")
-                ## I NEED TO FIX THIS! ##
-        else:
-            print("SPF record not in place!")
+            print('SPF in place', spfRecord)
+    # If no SPF record is available
+    if spfRecord is None:
+        print("SPF record not in place!")
+    # If SPF is present and configured with HardFail -
+    elif "-all" in spfRecord:
+        score+=2
+        print("HardFail inplace")
+    # Is SPF is present and configured with SoftFail ~
+    else:
+        score+1
+        print("SoftFail inplace")
+    # Prints the total score of the SPF test
+    print('Your SPF score is: ', score)
 
-    print('your score is: ',score)
-
-
+# If there is not such domain
 except (dns.resolver.NXDOMAIN):
     print('There is no such domain')
+# If there are not TXT records available for that domain
 except (dns.resolver.NoAnswer):
     print ('There are no TXT records for this domain')
-
-
-
-#spfRecord = [s for s in rdata if "v=spf" in s]
-        #$spfRecord = [txt_string for txt_string in rdata.strings if "v=spf" in txt_string]
-    # print('TXT:', rdata.to_text())
-    # print(' query qname:', answers.qname, ' num ans.', len(answers))
