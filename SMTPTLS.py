5154b99124657b59e113add278ab5479433220a4
ronv <ronvalensi@gmail.com>
2017-12-31 12:27:49 +0200

    DMARC.PY - score & prints SPF.PY - score & prints

--- SMTPTLS.py
+++ SMTPTLS.py
@@ -1,5 +1,26 @@
 
 
-# Import module
-import smtplib
+# Modules
+import dns.resolver
+import  smtplib
+from smtplib import SMTP
 
+# Reset: mxRecords, score?
+mxRecords = None
+maxScore = 3
+totalScore = 0
+
+# user input: Domain name
+#domain = input("Enter domain for SMTP TLS check:")
+
+splitMX = "40.68.241.53"
+try:
+    conn = SMTP('12324353143413412414')
+    #conn.ehlo()
+
+
+    #if conn.has_extn('STARTTLS'):
+     #   print('Domain support STARTTLS')
+
+except(smtplib.SMTPConnectError):
+    print("Unable to establish SMTP connection")
