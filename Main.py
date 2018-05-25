cf1532af0e5dc472b19a8f08513bb9f1c6481ace
ronv <ronvalensi@gmail.com>
2018-05-25 17:03:59 +0300

    Logic

--- Main.py
+++ Main.py
@@ -10,14 +10,48 @@ import json
 # get domain by init or by method SPFcheck
 #TODO: one dicetonery MX and IP and save result. save 2nd run for the same MX
 
+domains_list = ['valensiweb.com','chukustar.com','ronandsharontestdomain.com']
+aRecords = ['1.1.1.1','2.2.2.2','1.1.1.1','3.3.3.3']
+split_mx = 'MX Record Exmaple'
+output='just some info for tests%55'
+
+def update_from_IP_dict(dict_to_update,ip_results_dict,domain,ip,test_name):
+    score = (ip_results_dict[ip][testname]).get("score", "")
+    info = (ip_results_dict[ip][test_name]).get("info", "")
+    if ip not in dict_to_upate[domain]:
+        dict_to_update[domain][ip] = {}
+        dict_to_update[domain][ip]['tests'] = {}
+        dict_to_update[domain][ip]['tests'][test_name] = {'score': score,
+                                                       'info': info,
+                                                       'info_json': info}
+        return (dict_to_update)
+
+def get_mx(domain):
+    try:
+        mx_records = dns.resolver.query(domain, 'MX')
+        return(mx_records)
+    # If there is no such domain
+    except (dns.resolver.NXDOMAIN):
+        return(dns.resolver.NXDOMAIN)
+
+    # If there are no MX records available for that domain
+    except (dns.resolver.NoAnswer):
+        print('There are no MX records for domain', domain)
+        return (dns.resolver.NoAnswer)
+
+    # If no nameServers available
+    except (dns.resolver.NoNameservers):
+        return (dns.resolver.NoNameservers)
+
 def DicBuild(domains_list):
     ip_results_dict = {}
-
+    domains_dict = {}
     # Top enumeration through all domains
     for domain in domains_list:
         domains_dict[domain] = {}
+        domains_dict[domain]['tests'] = {}
         # Calling the SPF test for domain
-        output = SPFoop(domain)
+        #output = SPFoop(domain)
         # Adds SPF test output to the dictionary.
         domains_dict[domain]['tests']['SPF'] = {'grade': (output.split('%'))[1], 'info': (output.split('%'))[0],
                                       'info_json': (output.split('%'))[0]}
@@ -26,8 +60,10 @@ def DicBuild(domains_list):
         #domains_dict[domain]['MX'] = {'grade':(output.split('%'))[1],'info':(output.split('%'))[0],'info_json':(output.split('%'))[0]}
 
         try:
+            mxResults = get_mx(domain)
+            if mxResults # ADVANCE HERE
             # Querying for the domain's MX records
-            mx_records = dns.resolver.query(domain_name, 'MX')
+            mx_records = dns.resolver.query(domain, 'MX')
 
             # Enumerates through all the domain's MX records
             for rdata in mx_records:
@@ -46,7 +82,7 @@ def DicBuild(domains_list):
 
                             domains_dict[domain][adata] = {'mx':split_mx,'tests':{}}
                             # SMTPTLS
-                            output=smtptls(adata)
+                            #output=smtptls(adata)
                             domains_dict[domain][adata]['tests']['SMTPTLS'] = {'grade': (output.split('%'))[1], 'info': (output.split('%'))[0],
                                       'info_json': (output.split('%'))[0]}
 
@@ -54,16 +90,17 @@ def DicBuild(domains_list):
                                                                  'info': (output.split('%'))[0],
                                                                  'info_json': (output.split('%'))[0]}
                         else:
-                            score = (ip_results_dict[adata]['SMTPTLS']).get("score", "")
-                            info = (ip_results_dict[adata]['SMTPTLS']).get("info", "")
+                            update_from_IP_dict(domains_dict,ip_results_dict,domain,adata,'SMTPTLS')
+                            #score = (ip_results_dict[adata]['SMTPTLS']).get("score", "")
+                            #info = (ip_results_dict[adata]['SMTPTLS']).get("info", "")
 
-                            if adata not in domains_dictt[domain]:
-                                domains_dict[domain][adata] = {}
-                                domains_dict[domain][adata]['tests'] = {}
+                            #if adata not in domains_dictt[domain]:
+                             #  domains_dict[domain][adata] = {}
+                              #  domains_dict[domain][adata]['tests'] = {}
 
-                            domains_dict[domain][adata]['tests']['SMTPTLS'] = {'score': score,
-                                                                               'info': info,
-                                                                               'info_json': info}
+                            #domains_dict[domain][adata]['tests']['SMTPTLS'] = {'score': score,
+                             #                                                  'info': info,
+                              #                                                 'info_json': info}
 
                 # If there is no such domain
                 except (dns.resolver.NXDOMAIN):
@@ -84,15 +121,12 @@ def DicBuild(domains_list):
         except (dns.resolver.NoNameservers):
             print('No name servers found')
 
-        with open('domains.json', 'w') as data_file:
-            json.dump(domains_dict, data_file)
-            data_file.close
     #dmp = json.dump(domains_dict,fp="c:/temp/dump.txt")
     #for x in domains_dict:
     #    for y in domains_dict[x]:
     #        print(y, ':', domains_dict[x][y])
 
-    print (json.dump(domains_dict, indent=4, sort_keys=True))
+        #print(json.dumps(domains_dict, indent=4))
 
 domains_list = ['valensiweb.com','chukustar.com','ronandsharontestdomain.com']
 DicBuild(domains_list)
