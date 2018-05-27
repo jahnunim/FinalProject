3c36b123305777ec3f76535c07e216ea3fe74e72
ronv <ronvalensi@gmail.com>
2018-05-27 16:51:56 +0300

    Logic

--- Main.py
+++ Main.py
@@ -1,37 +1,64 @@
 print("Main")
+
+# Imports
 import SPFoop
 import dns.resolver
 import json
 
-# get domain list from file?? RestAPI??
-# initilize log file. where?
+
+
+# Consts#
+# Test names:
+SPF = 'SPF'
+DMARC = 'DMARC'
+MX = 'MX'
+OPENRELAY = 'OpenRelay'
+SMTPTLS = 'SMTPTLS'
+REVERSEDNS = 'REVERSEDNS'
+VRFY = 'VRFY'
+BLSPAMHAUS = 'BlackListSpamhaus'
+
+
+# initialize log file. where?
 # coding style - phyton
-# Rest API
+
 # get domain by init or by method SPFcheck
 #TODO: one dicetonery MX and IP and save result. save 2nd run for the same MX
 
-domains_list = ['valensiweb.com','chukustar.com','ronandsharontestdomain.com']
-#aRecords = ['1.1.1.1','2.2.2.2','1.1.1.1','3.3.3.3']
-#split_mx = 'MX Record Example'
+# Output to test
 output='just some info for tests%55'
 
-# Update the primary dictionary from IPs dictionary
+#############################################################################################################################
+# Update the primary dictionary with the results from the IPs dictionary.
+# The rational is to save tests on IPs that were already tested.
+# Input: dict_to_update - primary dictionary to update
+# ip_results_dict - the IPs dictionary with the results.
+# domain - which needs to be updated, ip - the smtp server's ip, test_name - the name of the test which called the update.
+##############################################################################################################################
 def update_from_IP_dict(dict_to_update,ip_results_dict,domain,ip,test_name):
+    # Gets the score and information of the tests, from the IP results dictionary
     score = (ip_results_dict[ip][test_name]).get("score", "")
     info = (ip_results_dict[ip][test_name]).get("info", "")
-    #if ip not in dict_to_upate[domain]:
+
+    # Updates the primary dictionary with the results
     dict_to_update[domain][ip] = {}
     dict_to_update[domain][ip]['tests'] = {}
     dict_to_update[domain][ip]['tests'][test_name] = {'score': score,
                                                     'info': info,
                                                     'info_json': info}
+    # returns dictionary to update
     return (dict_to_update)
 
-# GET MX for domain
+#####################################################
+# Get MX records for domain
+# INPUT: domain to pull the MX records for.
+####################################################
 def get_mx(domain):
     try:
+        # Queries the DNS for the MX records
         mx_records = dns.resolver.query(domain, 'MX')
         return(mx_records)
+
     # If there is no such domain
     except (dns.resolver.NXDOMAIN):
         return(dns.resolver.NXDOMAIN)
@@ -45,7 +72,13 @@ def get_mx(domain):
     except (dns.resolver.NoNameservers):
         return (dns.resolver.NoNameservers)
 
+############################################
+# Get server IPs behind the MX records
+# INPUT: list of mx_records
+#Output: dictonary mapping KEY=IP, VALUE=MX
+############################################
 def get_ip_from_mx(mx_records):
+    # init dic
     mx_to_ip_dict = {}
 
     # Enumerates through all the domain's MX records
@@ -56,26 +89,42 @@ def get_ip_from_mx(mx_records):
         # Query for A records
         aRecords = dns.resolver.query(split_mx,'A')
 
-
-
         # Enumerates through all A records behind the MX record.
         for ip in aRecords:
-            print(ip.to_text())
+            # Add the IP to dictionary as a key.
             mx_to_ip_dict[ip.address] = split_mx
 
     return(mx_to_ip_dict)
 
+#########################
+#update_ip_dict_results
+# Updates the IP dictionary with results
+#######################
+def update_ip_dict_results(ip_dict,ip,test_name,result):
+    ip_dict[ip][test_name] = {'score': (result.split('%'))[1],
+                                          'info': (result.split('%'))[0],
+                                          'info_json': (result.split('%'))[0]}
+    return(ip_dict)
 
+##################################################################################
+# This is the logic of building the primary Dictionary.
+# This Logic calls for each test and returns the final Output of the module.
+# Input: list of domains.
+# Output: Dictionary to be dump as a json file.
+################################################################################
 def DicBuild(domains_list):
+    # Init dictionaries
     ip_results_dict = {}
     domains_dict = {}
+
     # Top enumeration through all domains
     for domain in domains_list:
         domains_dict[domain] = {}
 
-        # Func: query the MX records behind the domain
+        # Func: returns the MX records behind the domain
         mxResults = get_mx(domain)
 
+        # Checks if the MX passed sucessfully.
         if (mxResults == dns.resolver.NoNameservers):
             domains_dict[domain] = {'Error':'No DNS server found'}
             break
@@ -88,19 +137,31 @@ def DicBuild(domains_list):
 
         # Get all IPs behind the MX record of the domain
         ips_dict = get_ip_from_mx(mxResults)
-        print (ips_dict)
 
-        # Tests in DOMAIN level: SPF, DMARC, MX
+        ########################################
+        # Tests in DOMAIN level: SPF, DMARC, MX#
+        ########################################
 
         # Init
         domains_dict[domain]['tests'] = {}
 
-        # SPF logic
+        #############
+        # SPF logic #
+        #############
         #output = SPFoop(domain)
+        #Updates primary dictionary.
         domains_dict[domain]['tests']['SPF'] = {'grade': (output.split('%'))[1], 'info': (output.split('%'))[0],
                                       'info_json': (output.split('%'))[0]}
-
+        ################
+        # DMARC logic #
+        ################
+        #TODO: Call DMARC # output = DMARC()
         #domains_dict[domain]['DMARC'] = {'grade':(output.split('%'))[1],'info':(output.split('%'))[0],'info_json':(output.split('%'))[0]}
+
+        #############
+        # MX logic #
+        #############
+        #TODO: Call MX # output = MX()
         #domains_dict[domain]['MX'] = {'grade':(output.split('%'))[1],'info':(output.split('%'))[0],'info_json':(output.split('%'))[0]}
 
         # Tests in IP level: OpenRelay, SMTPTLS, VRFY
@@ -109,100 +170,41 @@ def DicBuild(domains_list):
                 ip_results_dict[ip_key] = {}
                 domains_dict[domain][ip_key] = {'mx': ips_dict[ip_key], 'tests': {}}
 
-                # TESTS #
-                # SMTPTLS #
+                ##################
+                # IP based tests #
+                ##################
 
+                #################
+                # SMTPTLS logic #
+                #################
+                # TODO: SMTPTLS call
                 # output=smtptls(adata)
+                # updates primary dictionary
                 domains_dict[domain][ip_key]['tests']['SMTPTLS'] = {'grade': (output.split('%'))[1],
                                                                    'info': (output.split('%'))[0],
                                                                    'info_json': (output.split('%'))[0]}
 
+                # In case using update_ip_dict func will be used
+                #ip_results_dict = update_ip_dict_results(ip_results_dict,ip,'SMTPTLS',output)
+
+                # Updates the IP_RESULTS dictionary
                 ip_results_dict[ip_key]['SMTPTLS'] = {'score': (output.split('%'))[1],
                                                      'info': (output.split('%'))[0],
                                                      'info_json': (output.split('%'))[0]}
             else:
-                update_from_IP_dict(domains_dict, ip_results_dict, domain, ip_key, 'SMTPTLS')
-
-
-         # ADVANCE HERE
-        # Querying for the domain's MX records
-        #mx_records = dns.resolver.query(domain, 'MX')
-
-        # Calling the SPF test for domain
-        # Adds SPF test output to the dictionary.
-
-
-
-                    # # Enumerates through all A records behind the MX record.
-                    # for adata in aRecords:
-                    #     if adata not in ip_results_dict:
-                    #         ip_results_dict[adata] = {}
-                    #
-                    #         domains_dict[domain][adata] = {'mx':split_mx,'tests':{}}
-                    #         # SMTPTLS
-                    #         #output=smtptls(adata)
-                    #         domains_dict[domain][adata]['tests']['SMTPTLS'] = {'grade': (output.split('%'))[1], 'info': (output.split('%'))[0],
-                    #                   'info_json': (output.split('%'))[0]}
-                    #
-                    #         ip_results_dict[adata]['SMTPTLS'] = {'score': (output.split('%'))[1],
-                    #                                              'info': (output.split('%'))[0],
-                    #                                              'info_json': (output.split('%'))[0]}
-                    #     else:
-                    #         update_from_IP_dict(domains_dict,ip_results_dict,domain,adata,'SMTPTLS')
-
+                # Updates the primary dictionary from the IP results dictionary
+                domains_dict = update_from_IP_dict(domains_dict, ip_results_dict, domain, ip_key, 'SMTPTLS')
 
     return(domains_dict)
-                            #score = (ip_results_dict[adata]['SMTPTLS']).get("score", "")
-                            #info = (ip_results_dict[adata]['SMTPTLS']).get("info", "")
 
-                            #if adata not in domains_dictt[domain]:
-                             #  domains_dict[domain][adata] = {}
-                              #  domains_dict[domain][adata]['tests'] = {}
-
-                            #domains_dict[domain][adata]['tests']['SMTPTLS'] = {'score': score,
-                             #                                                  'info': info,
-                              #                                                 'info_json': info}
-
-        #         # If there is no such domain
-        #         except (dns.resolver.NXDOMAIN):
-        #             print('There is no domain', split_mx)
-        #         # If there are no A records available for that domain
-        #         except (dns.resolver.NoAnswer):
-        #             print('There are no A records for MX', split_mx)
-        #
-        # # If there is no such domain
-        # except (dns.resolver.NXDOMAIN):
-        #     print('There is no domain', domain)
-        #
-        # # If there are no MX records available for that domain
-        # except (dns.resolver.NoAnswer):
-        #     print('There are no MX records for domain', domain)
-        #
-        # # If no nameServers available
-        # except (dns.resolver.NoNameservers):
-        #     print('No name servers found')
-
-    #dmp = json.dump(domains_dict,fp="c:/temp/dump.txt")
-    #for x in domains_dict:
-    #    for y in domains_dict[x]:
-    #        print(y, ':', domains_dict[x][y])
-
-        #print(json.dumps(domains_dict, indent=4))
 
 #domains_list = ['valensiweb.com','chukustar.com','ronandsharontestdomain.com']
-domains_list = ['valensiweb.com','chukustar.com','tomersegal.com','segale5.onmicrosoft.com']
+domains_list = ['valensiweb.com','chukustar.com','tomersegal.com','segale5.onmicrosoft.com','ronandsharontestdomain.com']
 final_dict = DicBuild(domains_list)
 
 print(json.dumps(final_dict,indent=4))
 
-#p1 = SPFoop.SPF('valensiweb.com')
-#p1.SPFcheck('valensiweb.com')
-#p4 = SPFoop.SPF('RonandSharonnosuchdomain.com')
-#p4.SPFcheck('RonandSharonnosuchdomain.com')
-#p2 = SPFoop.SPF('nice.com')
-#p2.SPFcheck('nice.com')
-#p3 = SPFoop.SPF('chukustar.com')
-#p3.SPFcheck('chukustar.com')
+
 
 
 #TODO: closing file and delete
@@ -213,11 +215,73 @@ print(json.dumps(final_dict,indent=4))
 #   p = SPFoop(domain)
 #   p.SPFCheck(domain)
 
+#p1 = SPFoop.SPF('valensiweb.com')
+#p1.SPFcheck('valensiweb.com')
+#p4 = SPFoop.SPF('RonandSharonnosuchdomain.com')
+#p4.SPFcheck('RonandSharonnosuchdomain.com')
+#p2 = SPFoop.SPF('nice.com')
+#p2.SPFcheck('nice.com')
+#p3 = SPFoop.SPF('chukustar.com')
+#p3.SPFcheck('chukustar.com')
+
+# score = (ip_results_dict[adata]['SMTPTLS']).get("score", "")
+# info = (ip_results_dict[adata]['SMTPTLS']).get("info", "")
+
+# if adata not in domains_dictt[domain]:
+#  domains_dict[domain][adata] = {}
+#  domains_dict[domain][adata]['tests'] = {}
+
+# domains_dict[domain][adata]['tests']['SMTPTLS'] = {'score': score,
+#                                                  'info': info,
+#                                                 'info_json': info}
+
+#         # If there is no such domain
+#         except (dns.resolver.NXDOMAIN):
+#             print('There is no domain', split_mx)
+#         # If there are no A records available for that domain
+#         except (dns.resolver.NoAnswer):
+#             print('There are no A records for MX', split_mx)
+#
+# # If there is no such domain
+# except (dns.resolver.NXDOMAIN):
+#     print('There is no domain', domain)
+#
+# # If there are no MX records available for that domain
+# except (dns.resolver.NoAnswer):
+#     print('There are no MX records for domain', domain)
+#
+# # If no nameServers available
+# except (dns.resolver.NoNameservers):
+#     print('No name servers found')
+
+# dmp = json.dump(domains_dict,fp="c:/temp/dump.txt")
+# for x in domains_dict:
+#    for y in domains_dict[x]:
+#        print(y, ':', domains_dict[x][y])
+
+# print(json.dumps(domains_dict, indent=4))
+
+# # Enumerates through all A records behind the MX record.
+# for adata in aRecords:
+#     if adata not in ip_results_dict:
+#         ip_results_dict[adata] = {}
+#
+#         domains_dict[domain][adata] = {'mx':split_mx,'tests':{}}
+#         # SMTPTLS
+#         #output=smtptls(adata)
+#         domains_dict[domain][adata]['tests']['SMTPTLS'] = {'grade': (output.split('%'))[1], 'info': (output.split('%'))[0],
+#                   'info_json': (output.split('%'))[0]}
+#
+#         ip_results_dict[adata]['SMTPTLS'] = {'score': (output.split('%'))[1],
+#                                              'info': (output.split('%'))[0],
+#                                              'info_json': (output.split('%'))[0]}
+#     else:
+#         update_from_IP_dict(domains_dict,ip_results_dict,domain,adata,'SMTPTLS')
 
 
+# ADVANCE HERE
+# Querying for the domain's MX records
+# mx_records = dns.resolver.query(domain, 'MX')
 
-
-
-
-
-
+# Calling the SPF test for domain
+# Adds SPF test output to the dictionary.
