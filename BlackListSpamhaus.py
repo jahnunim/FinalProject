ab670abb03a398ee3c45529ef5649e7f6d00e2b1
SharonStone <stone.332.sharon@gmail.com>
2018-05-29 21:41:35 +0300

    ...

--- BlackListSpamhaus.py
+++ BlackListSpamhaus.py
@@ -6,72 +6,57 @@
 
 # Modules
 import dns.resolver
+import TestGen
 
-# Init
-bl = "zen.spamhaus.org"
-#myIP = "14.114.37.4"
-mxRecord = None
-aRecord = None
-ptrRecord = None
-maxScore = 0
-totalScore = 0
+class BlackList(TestGen.Test):
+    name = 'BlackList'
+    #TODO: send spf_object the class and the logging will take the class object
+    def __init__(self, ip_string):
+        self.ip_string = ip_string
+        TestGen.Test.InitialLog(self)
 
-# user input: Domain name
-domain = input("Enter domain for black list check:")
+    def BlackListcheck(self, ip_string):
+        # Init
+        bl = "zen.spamhaus.org"
+        #myIP = "14.114.37.4"
+        mxRecord = None
+        aRecord = None
+        ptrRecord = None
+        maxScore = 0
+        totalScore = 0
+        info_send = ""
+        bl_object = BlackList(ip_string)
 
-# Query for the domain MX
-try:
-    # Querying for the domain's MX records
-    mxRecord = dns.resolver.query(domain, 'MX')
-
-    # Enumaretes through all the domain's MX records
-    for rdata in mxRecord:
-        splitMX = (rdata.to_text().split(" "))[1]
-        splitMX = splitMX[:-1]
-
-        # Pulls the A records behind the MX records
+        # Query for Spamhaus zone, if no result returns - IP not listed in black list
         try:
-            aRecord = dns.resolver.query(splitMX,'A')
+            # New DNS resolver
+            my_resolver = dns.resolver.Resolver()
 
-            # for each A record, check if a PTR record is present.\
-            for adata in aRecord:
-                # Query for Spamhaus zone, if no result returns - IP not listed in black list
-                try:
-                    # New DNS resolver
-                    my_resolver = dns.resolver.Resolver()
+            # Builds the query
+            query = '.'.join(reversed(str(ip_string).split("."))) + "." + bl
 
-                    # Builds the query
-                    query = '.'.join(reversed(str(adata).split("."))) + "." + bl
+            # Queries Spamhaus DNS.
+            answers = my_resolver.query(query,"A")
 
-                    # Queries Spamhaus DNS.
-                    answers = my_resolver.query(query,"A")
+            # If no exception was triggered, IP is listed in bl.
+            # Gets the TXT record to build te link for details.
+            maxScore +=1
+            answer_txt = my_resolver.query(query,"TXT")
+            info_send += 'IP: %s IS listed in %s (%s: %s)' % (adata, bl, answers[0], answer_txt[0])
+            info = 'IP: %s IS listed in %s (%s: %s)' % (adata, bl, answers[0], answer_txt[0])
+            bl_object.Log(ip_string, 'BlackListSpamhaus', info)
+        # Exception raised, IP not listed in BL.
+        except(dns.resolver.NXDOMAIN):
+            maxScore += 1
+            totalScore +=1
+            info_send += 'IP: %s is NOT listed in %s' % (adata, bl)
+            info = 'IP: %s is NOT listed in %s' % (adata, bl)
+            bl_object.Log(ip_string, 'BlackListSpamhaus', info)
 
-                    # If no exception was triggered, IP is listed in bl.
-                    # Gets the TXT record to build te link for details.
-                    maxScore +=1
-                    answer_txt = my_resolver.query(query,"TXT")
-                    print('IP: %s IS listed in %s (%s: %s)' % (adata, bl, answers[0], answer_txt[0]))
+        score_result = bl_object.Score(totalScore, maxScore)
+        score_result = "%" + score_result.__str__()
+        bl_object.Log(ip_string, 'SCORE VRFY', score_result)
+        info_send += score_result
+        TestGen.testID += 1
 
-                # Exception raised, IP not listed in BL.
-                except(dns.resolver.NXDOMAIN):
-                    maxScore += 1
-                    totalScore +=1
-                    print('IP: %s is NOT listed in %s' % (adata, bl))
-
-        # If there are no MX records available for that domain
-        except (dns.resolver.NoAnswer):
-            print('There are no A records for domain', mxRecord)
-
-    print('Total ReverseDNS score for domain', domain, 'is', totalScore, '/', maxScore)
-
-# If there is no such domain
-except (dns.resolver.NXDOMAIN):
-    print('There is no domain', domain)
-
-# If there are no MX records available for that domain
-except (dns.resolver.NoAnswer):
-    print('There are not MX records for domain', domain)
-
-# If no nameServers available
-except (dns.resolver.NoNameservers):
-    print('No name servers found')
+        return (info_send)
